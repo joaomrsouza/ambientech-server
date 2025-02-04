7f3758749d76d0d32089f21955cf393ee6e8430f
AnaLara <analara714@gmail.com>
2025-02-03 22:53:10 -0300

    ajusta a data da consulta de dados para o grafico

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,20 +2,18 @@ import type { Request, Response } from "express";
 import { prisma } from "../database";
 
 export async function root(_req: Request, res: Response) {
-  const today = new Date().toISOString().split("T")[0]! + "T00:00";
-  const ontem =
-    new Date(new Date(today).setDate(new Date(today).getDate() - 1))
-      .toISOString()
-      .split("T")[0]! + "T00:00";
+  const localDate = new Date(
+    Date.now() - new Date().getTimezoneOffset() * 60000
+  ).toISOString();
+  const today = localDate.split("T")[0]! + "T00:00:00.000-03:00";
 
   const where = {
     timestamp: {
-      gte: new Date(ontem),
-      lt: new Date(new Date(ontem).setDate(new Date(ontem).getDate() + 1)),
+      gte: new Date(today),
+      lt: new Date(new Date(today).setDate(new Date(today).getDate() + 1)),
     },
     value: { not: 0 },
   };
-
   const [temperatureData, humidityData, rainData, qOAData, smokeData] =
     await Promise.all([
       prisma.temperatureData.findMany({ where }),

e8d349250fd2f36a92ef6ead69572f714082d6aa
João Marcos Rocha Souza <joaomarcos25@hotmail.com>
2025-02-04 22:06:12 -0300

    Organiza inicialização do servidor. Ajusta o timestamp da query para funcionar no servidor.

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,3 @@
-import { type Express } from "express";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
-
-export async function databaseConnect(app: Express) {
-  console.log("Connecting to database");
-  await prisma.$connect();
-  console.log("Database connected");
-  app.emit("ready");
-}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import express from "express";
 import { join } from "path";
 
 import { router } from "./routes";
-import { databaseConnect, prisma } from "./database";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// databaseConnect(app);
 
 app.use(express.static(join(__dirname, "../public")));
 app.use(express.json());
@@ -16,43 +13,39 @@ app.set("view engine", "pug");
 
 app.use(router);
 
-app.on("ready", () => {
-  try {
-    console.log("Server: Server is ready");
-    app.listen(PORT, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Server: Server is listening on port ${PORT}`);
-    });
-    if (process.env.IS_PRODUCTION === "true") {
-      // eslint-disable-next-line no-console
-      console.log("Starting Jobs");
-      // TODO: startJobs();
-    }
+console.log("Server: Server is ready");
 
-    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
-
-    for (const sig of exitSignals) {
-      process.on(sig, async () => {
-        try {
-          // await stopJobs();
-          await prisma.$disconnect();
-          console.info("App exited with success");
-          process.exit(ExitStatus.Success);
-        } catch (error) {
-          console.error(`App exited with error: ${error}`);
-          process.exit(ExitStatus.Failure);
-        }
-      });
-    }
-  } catch (error) {
-    console.error(`App exited with error: ${error}`);
-    process.exit(ExitStatus.Failure);
-  }
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server: Server is listening on port ${PORT}`);
 });
+
+if (process.env.IS_PRODUCTION === "true") {
+  // eslint-disable-next-line no-console
+  console.log("Starting Jobs");
+  // TODO: startJobs();
+}
+
+// Process exit/error handlers
 
 enum ExitStatus {
   Failure = 1,
   Success = 0,
+}
+
+const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+
+for (const sig of exitSignals) {
+  process.on(sig, async () => {
+    try {
+      // TODO: await stopJobs();
+      console.info("App exited with success");
+      process.exit(ExitStatus.Success);
+    } catch (error) {
+      console.error(`App exited with error: ${error}`);
+      process.exit(ExitStatus.Failure);
+    }
+  });
 }
 
 process.on("unhandledRejection", (reason, promise) => {
@@ -66,4 +59,3 @@ process.on("uncaughtException", (error) => {
   console.error(`App exiting due to an uncaught exception: ${error}`);
   process.exit(ExitStatus.Failure);
 });
-app.emit("ready");

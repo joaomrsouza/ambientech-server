ddb63fcc095a3b916abb051be3274aef1fda69e2
João Marcos Rocha Souza <joaomarcos25@hotmail.com>
2025-02-05 15:24:57 -0300

    Debug timestamp query em produção

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,67 +1,20 @@
 import type { Request, Response } from "express";
 import { prisma } from "../database";
-import { subHours } from "date-fns";
+import { addDays, setHours, subDays, subHours } from "date-fns";
 
 export async function root(_req: Request, res: Response) {
-  const hj = new Date();
-  const hjm3 = subHours(hj, 3);
-  const hjm300 = new Date(hjm3.setHours(0, 0, 0, 0));
-  console.log(
-    JSON.stringify(
-      {
-        hj,
-        hjm3,
-        hjm300,
-        hjm300String: hjm300.toString(),
-        hjm300ISO: hjm300.toISOString(),
-      },
-      null,
-      2
+  // const correctedDate =
+  const today = new Date(
+    subHours(
+      new Date().setHours(0, 0, 0, 0),
+      new Date().getTimezoneOffset() / 60
     )
   );
-
-  const dateFromServer = new Date(); // California midnight
-  const serverOffset = new Date().getTimezoneOffset(); // in minutes, from that API call
-  const serverOffsetMillis = 60 * 1000 * serverOffset;
-  const localOffset = -60 * 3; // in minutes
-  const localOffsetMillis = 60 * 1000 * localOffset;
-  const localMidnight = new Date(
-    dateFromServer.getTime() - serverOffsetMillis + localOffsetMillis
-  );
-  console.log(
-    JSON.stringify(
-      {
-        dateFromServer,
-        serverOffset,
-        serverOffsetMillis,
-        localOffset,
-        localOffsetMillis,
-        localMidnight,
-        localMidnightString: localMidnight.toString(),
-      },
-      null,
-      2
-    )
-  );
-
-  const localDate = new Date(
-    new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-      .toISOString()
-      .replace("z", "+03:00")
-  ).toISOString();
-  const today = localDate.split("T")[0]! + "T00:00:00.000";
-
-  const yesterday = new Date(
-    new Date(today).setDate(new Date(today).getDate() - 1)
-  );
-
-  const correctedDate =
-    process.env.NODE_ENV === "production" ? yesterday : today;
 
   const where = {
     timestamp: {
-      gte: new Date(today),
-      lt: new Date(new Date(today).setDate(new Date(today).getDate() + 1)),
+      gte: today,
+      lt: addDays(today, 1), // new Date(today.setDate(today.getDate() + 1)),
     },
     value: { not: 0 },
   };
@@ -69,11 +22,10 @@ export async function root(_req: Request, res: Response) {
   console.log(
     JSON.stringify(
       {
-        localDate,
+        env: process.env.NODE_ENV,
+        // correctedDate,
         today,
-        correctedDate,
         where,
-        dateToday: new Date(localDate.replace("Z", "+03:00")),
         localOffset: new Date().getTimezoneOffset(),
       },
       null,
